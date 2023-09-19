c718dd1f18f8b36090aabef11fc36ce666b19893
harshvachhani1 <harshv@webelight.co.in>
2023-09-19 17:40:52 +0530

    Data type Example apply code of null and undefined type

---
+++ Function/Undefined_and_Null.js
@@ -0,0 +1,23 @@
+// Undefined for variable Ex
+
+let name;
+// name ="hello";
+if (name === undefined) {
+  console.log("please enter correct name");
+} else {
+  console.log(name);
+}
+
+// Undefined for argument Ex
+
+let sum = (eng) => console.log(eng);
+sum();
+
+// Null Ex
+// const var1 = 56;
+const var1 = null;
+if (var1) {
+  console.log("var1 is not null");
+} else {
+  console.log("var1 is null");
+}
