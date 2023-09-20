dd0870913a7e3676f616d008c56d848a34331e5e
harshvachhani1 <harshv@webelight.co.in>
2023-09-20 17:01:21 +0530

    change type let to const

--- Function/Grade_Calculator.js
+++ Function/Grade_Calculator.js
@@ -1,17 +1,17 @@
-let math = 90;
-let sci = 80;
-let guj = 50;
+const math = 90;
+const sci = 80;
+const guj = 50;
 
-let subject = (sub1, sub2, sub3) => sub1 + sub2 + sub3;
-var TotalMark = subject(math, sci, guj);
+const subject = (sub1, sub2, sub3) => sub1 + sub2 + sub3;
+const TotalMark = subject(math, sci, guj);
 console.log(`Three Subject Mark is : ${TotalMark}`);
 
-let percentage = (SubjectMark) => (SubjectMark / 300) * 100;
-var percentages = percentage(TotalMark);
+const percentage = (SubjectMark) => (SubjectMark / 300) * 100;
+const percentages = percentage(TotalMark);
 console.log(`Your Percentages is : ${percentages}`);
 
 //while below comment code enable then this code apply in comment
-let Grade = (Mathematics, science, gujarati, percentages) => {
+const Grade = (Mathematics, science, gujarati, percentages) => {
   if (Mathematics <= 35 || science <= 35 || gujarati <= 35) {
     Status = "You are fail ";
   } else if (percentages >= 80) {
@@ -23,8 +23,7 @@ let Grade = (Mathematics, science, gujarati, percentages) => {
   }
   return `your status is ${Status}`;
 };
-var grades = Grade(math, sci, guj, percentages);
-console.log(grades);
+console.log(Grade(math, sci, guj, percentages));
 
 // let Grade = (Mathematics, science, gujarati, percentages) => {
 //   if (Mathematics <= 35 || science <= 35 || gujarati <= 35) {
