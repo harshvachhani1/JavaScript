8b93cce2a5f20037e24e9189eea1879e48a6372b
harshvachhani1 <harshv@webelignt.co.in>
2023-09-25 16:53:06 +0530

    explain todos remove using event

---
+++ DOM/Advanced-Queries/notes-app.js
@@ -0,0 +1,32 @@
+const todos = [
+  {
+    text: "Order cat food",
+    completed: true,
+  },
+  {
+    text: "Clean kitchen",
+    completed: false,
+  },
+  {
+    text: "Buy food",
+    completed: true,
+  },
+  {
+    text: "Do work",
+    completed: false,
+  },
+  {
+    text: "Exercise",
+    completed: true,
+  },
+];
+
+document.querySelector("#create").addEventListener("click", (targetValue) => {
+  // targetValue.target.textContent = "Create Button Click";
+});
+
+document.querySelector("#remove").addEventListener("click", (targetValue) => {
+  document.querySelectorAll(".note").forEach(function (note) {
+    note.remove();
+  });
+});
